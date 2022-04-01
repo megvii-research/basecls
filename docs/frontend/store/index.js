4cb504babd4dbba85f91d9a449b66450277e792e
Xu Xinran <xxr@megvii.com>
2022-04-01 14:25:27 +0800

    feat(doc): change web api root address

--- docs/frontend/store/index.js
+++ docs/frontend/store/index.js
@@ -2,7 +2,7 @@ import { createStore } from "vuex";
 
 export default createStore({
     getters: {
-      APIBase: () => process.env.NODE_ENV !== 'production'?'':'https://megengine.org.cn/meta/basecls',
+      APIBase: () => process.env.NODE_ENV !== 'production'?'':'https://www.megengine.org.cn/meta/basecls',
       OSSPath: () => 'https://data.megengine.org.cn/research/basecls/models'
     }
 })
